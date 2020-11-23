1c94ba51aaad72798be898936953831d1f4977b5
one356 <one356@qq.com>
2020-11-23 10:24:12 +0800

    豆瓣电影抓取

--- bin/spyder-1/20201123.py
+++ bin/spyder-1/20201123.py
@@ -7,5 +7,24 @@
 """
 # 抓取豆瓣电影
 import requests
+import json
 if __name__ == '__main__':
-
+    url = 'https://movie.douban.com/j/chart/top_list'
+    param = {
+        'type': '24',
+        'interval_id': '100:90',
+        'action':'',
+        'start': '0',
+        'limit': '20',
+    }
+    headers = {
+               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36'
+               }
+    response = requests.get(url=url,headers=headers,params=param)
+    list_data = response.json()
+    fp = open('./movice.json','w',encoding='utf-8')
+    # 转换成字典
+    json.dump(list_data,fp=fp,ensure_ascii=False)
+    print('完成')
+    
+    
