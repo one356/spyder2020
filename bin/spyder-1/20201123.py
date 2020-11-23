17e6b22b1498d71d0392ac0949db107ef2a70cb6
one356 <one356@qq.com>
2020-11-23 08:01:43 +0800

    豆瓣电影抓取

---
+++ bin/spyder-1/20201123.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+    @author:Administrator
+    @file:20201123.py
+    @time:2020/11/23
+"""
+# 抓取豆瓣电影
+import requests
+if __name__ == '__main__':
+
